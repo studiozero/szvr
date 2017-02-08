6edcd5d309cc4bf2a3dddbd3f996a463fdd9bebf
Leigh Garland <leigh@studiozero.co>
2017-02-08 14:09:10 +0000

    Fixed up some templates to display version

--- config.js
+++ config.js
@@ -1,23 +1,28 @@
 var fs = require('fs-extra');
+var moment = require('moment');
+var info = require('./package.json');
 
 var srcDir = './src';
 var wwwDir = './www';
 var templateDir = './templates';
 var templateExt = '.pug';
 var canonicalRoot = 'http://studiozero.co';
+var datePattern = 'MMM do, YYYY'; // follows momentjs date formatting - https://momentjs.com/docs/#/parsing/string-format/
+var version = info.version || 0;
 
 var defaultData = {
 	"_paths" : {},
 	"_template" : "index.pug",
 	"_metadata" : {
-		date: new Date(),
+		date: moment().format(datePattern),
 		site : 'Studio Zero',
 		url: canonicalRoot, // but is overwritten by canonicalRoot + path for generated pages
 		image: canonicalRoot + '/assets/images/studiozero_og1200x1200.jpg',
 		title: 'We are Studio Zero', // keep it short
 		sell: 'We are Studio Zero | Immersive Web | VR | AR | MR | AI', // keep it < 80 chars
 		summary: 'A creative studio building VR products and experiences for the web.', // keep it < 120 chars
-		description: 'A creative studio building VR products and experiences for the web.' // keep it focused
+		description: 'A creative studio building VR products and experiences for the web.', // keep it focused
+		systemVersion : version
 	}
 };
 
@@ -27,5 +32,7 @@ module.exports = {
 	templateDir : fs.realpathSync(templateDir),
 	templateExt : templateExt,
 	defaultData : defaultData,
-	canonicalRoot : canonicalRoot
+	canonicalRoot : canonicalRoot,
+	datePattern : datePattern,
+	version : version
 }
